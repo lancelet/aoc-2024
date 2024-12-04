dc1335c171ad9e53d73b5f5ad5395e6f249b4dd4
Jonathan Merritt <j.s.merritt@gmail.com>
2024-12-04 13:04:07 +1100

    Day 1 - Part 2

--- haskell/src/P01.hs
+++ haskell/src/P01.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module P01 (main) where
 
 import Control.Applicative (many, some, (<|>))
@@ -19,7 +21,28 @@ main :: IO ()
 main = do
   (xs, ys) <- readInput
   let d = distance xs ys
-  putStrLn $ "Computed distance: " <> show d
+  let s = similarity xs ys
+  putStrLn $ "Computed distance:   " <> show d
+  putStrLn $ "Computed similarity: " <> show s
+
+---- Similarity score ---------------------------------------------------------
+
+-- This is a bit slow; but meh.
+similarity :: [Int] -> [Int] -> Int
+similarity xs ys = go 0 xs
+  where
+    go :: Int -> [Int] -> Int
+    go score [] = score
+    go score (e : es) = go (score + countElem e ys * e) es
+
+countElem :: forall a. (Eq a) => a -> [a] -> Int
+countElem item = go 0
+  where
+    go :: Int -> [a] -> Int
+    go count [] = count
+    go count (e : es)
+      | e == item = go (count + 1) es
+      | otherwise = go count es
 
 ---- Compute distance ---------------------------------------------------------
 
