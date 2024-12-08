dad0848caa2abe4b4302413bdc7350df700e7c8d
Jonathan Merritt <j.s.merritt@gmail.com>
2024-12-08 20:22:00 +1100

    Day 8 - Part 2

--- haskell/src/P08.hs
+++ haskell/src/P08.hs
@@ -21,7 +21,9 @@ main = do
   putStrLn "Day 8"
   inGrid <- readInput
   let part1Result = part1 inGrid
+  let part2Result = part2 inGrid
   putStrLn $ "Part 1 result: " <> show part1Result
+  putStrLn $ "Part 2 result: " <> show part2Result
 
 data Cell
   = CellEmpty
@@ -37,6 +39,9 @@ type AntiNodeGrid = Grid.Grid V.Vector Bool
 
 part1 :: InGrid -> Int
 part1 inGrid = Grid.countEqual True $ createAntiNodeGrid inGrid
+
+part2 :: InGrid -> Int
+part2 inGrid = Grid.countEqual True $ createHarmonicsGrid inGrid
 
 createAntiNodeGrid :: InGrid -> AntiNodeGrid
 createAntiNodeGrid inGrid =
@@ -57,12 +62,42 @@ createAntiNodeGrid inGrid =
           False
    in Grid.setMulti True answ32 emptyGrid
 
+createHarmonicsGrid :: InGrid -> AntiNodeGrid
+createHarmonicsGrid inGrid =
+  let rows, cols :: Int
+      rows = fromIntegral $ Grid.getRows inGrid
+      cols = fromIntegral $ Grid.getCols inGrid
+
+      locs :: [AntennaAndLocs]
+      locs = findAntennaLocs inGrid
+
+      harms :: [(Int, Int)]
+      harms = harmonicsForAntennas rows cols locs
+
+      answ32 :: [(Word32, Word32)]
+      answ32 = bimap fromIntegral fromIntegral <$> harms
+
+      emptyGrid :: AntiNodeGrid
+      emptyGrid =
+        Grid.filledWith
+          (Grid.getRows inGrid)
+          (Grid.getCols inGrid)
+          False
+   in Grid.setMulti True answ32 emptyGrid
+
 antinodesForAntennas :: [AntennaAndLocs] -> [(Int, Int)]
 antinodesForAntennas = concatMap antinodesForAntenna
 
 antinodesForAntenna :: AntennaAndLocs -> [(Int, Int)]
 antinodesForAntenna (AntennaAndLocs _ xs) =
   concatMap antiNodePair (uniquePairs xs)
+
+harmonicsForAntennas :: Int -> Int -> [AntennaAndLocs] -> [(Int, Int)]
+harmonicsForAntennas rows cols = concatMap (harmonicsForAntenna rows cols)
+
+harmonicsForAntenna :: Int -> Int -> AntennaAndLocs -> [(Int, Int)]
+harmonicsForAntenna rows cols (AntennaAndLocs _ xs) =
+  concatMap (harmonics rows cols) (uniquePairs xs)
 
 findAntennaLocs :: InGrid -> [AntennaAndLocs]
 findAntennaLocs inGrid =
@@ -85,6 +120,24 @@ antiNodePair ((x1, y1), (x2, y2)) =
    in [ (x1 - dx, y1 - dy),
         (x2 + dx, y2 + dy)
       ]
+
+harmonics :: Int -> Int -> ((Int, Int), (Int, Int)) -> [(Int, Int)]
+harmonics rows cols (p1, p2) =
+  p1
+    : p2
+    : directionalHarmonics rows cols (p1, p2)
+      <> directionalHarmonics rows cols (p2, p1)
+
+directionalHarmonics :: Int -> Int -> ((Int, Int), (Int, Int)) -> [(Int, Int)]
+directionalHarmonics rows cols ((x1, y1), (x2, y2)) =
+  let dx = x2 - x1
+      dy = y2 - y1
+      x3 = x2 + dx
+      y3 = y2 + dy
+      ptValid = (x3 >= 0) && (x3 < cols) && (y3 >= 0) && (y3 < rows)
+   in if ptValid
+        then (x3, y3) : directionalHarmonics rows cols ((x2, y2), (x3, y3))
+        else []
 
 ---- Parsing ------------------------------------------------------------------
 
