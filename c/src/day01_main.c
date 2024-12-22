39808e92bc241548e9c23f8cd837c6352bf14b46
Jonathan Merritt <j.s.merritt@gmail.com>
2024-12-22 16:46:30 +1100

    C formatting

--- c/src/day01_main.c
+++ c/src/day01_main.c
@@ -11,114 +11,118 @@ int list2[MAX_LIST_LENGTH];
 
 const char *input_file_name = "../inputs/input-day-01.txt";
 
-int read_input_lists(const char*, int, int*, int*);
-bool line_is_blank(const char*);
+int read_input_lists(const char *, int, int *, int *);
+bool line_is_blank(const char *);
 int compare_ints(const void *, const void *);
-int part1(const int, int*, int*);
+int part1(const int, int *, int *);
 int part2(const int n_elems, const int *list1, const int *list2);
 
 int main(void) {
-    printf("Day 1\n");
-    int n_elems = read_input_lists(input_file_name, MAX_LIST_LENGTH, list1, list2);
-    if (n_elems < 0) {
-        return -1;
-    }
-    printf("Number of input list elements = %i\n", n_elems);
+  printf("Day 1\n");
+  int n_elems =
+      read_input_lists(input_file_name, MAX_LIST_LENGTH, list1, list2);
+  if (n_elems < 0) {
+    return -1;
+  }
+  printf("Number of input list elements = %i\n", n_elems);
 
-    // Part 1
-    int sum_diff = part1(n_elems, list1, list2);
-    printf("Sum of differences: %i\n", sum_diff);
+  // Part 1
+  int sum_diff = part1(n_elems, list1, list2);
+  printf("Sum of differences: %i\n", sum_diff);
 
-    // Part 2
-    int part2_result = part2(n_elems, list1, list2);
-    printf("Part 2 result: %i\n", part2_result);
+  // Part 2
+  int part2_result = part2(n_elems, list1, list2);
+  printf("Part 2 result: %i\n", part2_result);
 
-    return 0;
+  return 0;
 }
 
 int part1(const int n_elems, int *list1, int *list2) {
-    // Sort both of the input lists; this mutates them.
-    qsort(list1, n_elems, sizeof(int), compare_ints);
-    qsort(list2, n_elems, sizeof(int), compare_ints);
+  // Sort both of the input lists; this mutates them.
+  qsort(list1, n_elems, sizeof(int), compare_ints);
+  qsort(list2, n_elems, sizeof(int), compare_ints);
 
-    // Sum up differences.
-    int sum = 0;
-    for (int i=0; i < n_elems; ++i) {
-        sum += abs(list1[i] - list2[i]);
-    }
-    return sum;
+  // Sum up differences.
+  int sum = 0;
+  for (int i = 0; i < n_elems; ++i) {
+    sum += abs(list1[i] - list2[i]);
+  }
+  return sum;
 }
 
 int part2(const int n_elems, const int *list1, const int *list2) {
-    assert(n_elems > 0);
-    int total = 0;
-    int list2_idx = 0;
-    for (int list1_idx=0; list1_idx < n_elems; ++list1_idx) {
-        int cur_elem = list1[list1_idx];
-        while (list2_idx < n_elems && list2[list2_idx] < cur_elem) {++list2_idx;}
-        int count = 0;
-        while ((list2_idx+count) < n_elems && list2[list2_idx+count] == cur_elem) {++count;}
-        total += count * cur_elem;
-        list2_idx += count;
+  assert(n_elems > 0);
+  int total = 0;
+  int list2_idx = 0;
+  for (int list1_idx = 0; list1_idx < n_elems; ++list1_idx) {
+    int cur_elem = list1[list1_idx];
+    while (list2_idx < n_elems && list2[list2_idx] < cur_elem) {
+      ++list2_idx;
     }
-    return total;
+    int count = 0;
+    while ((list2_idx + count) < n_elems &&
+           list2[list2_idx + count] == cur_elem) {
+      ++count;
+    }
+    total += count * cur_elem;
+    list2_idx += count;
+  }
+  return total;
 }
 
 int compare_ints(const void *va, const void *vb) {
-    int a = *(int *)va;
-    int b = *(int *)vb;
-    if (a < b) {
-        return -1;
-    } else if (a > b) {
-        return 1;
-    } else {
-        return 0;
-    }
+  int a = *(int *)va;
+  int b = *(int *)vb;
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-int read_input_lists(
-    const char *input_file_name,
-    int max_list_length,
-    int *list1,
-    int *list2
-) {
-    FILE *in = fopen(input_file_name, "r");
-    if (in == NULL) {
-        perror("Could not open file");
-        return -1;
-    }
+/**** PARSING ****************************************************************/
 
-    int n_lines_read = 0;
-    char line[MAX_INPUT_LINE_CHARS];
-    while (fgets(line, sizeof(line), in)) {
-        if (line_is_blank(line)) {
-            continue;
-        }
-        if (n_lines_read >= max_list_length) {
-            fprintf(stderr, "Input file exceeded maximum list length.\n");
-            fclose(in);
-            return -1;
-        }
-        int a, b;
-        if (sscanf(line, "%d %d", &a, &b) == 2) {
-            list1[n_lines_read] = a;
-            list2[n_lines_read] = b;
-            ++n_lines_read;
-        } else {
-            fprintf(stderr, "Could not read two int values from line.\n");
-            fclose(in);
-            return -1;
-        }
-    }
-    if (ferror(in)) {
-        perror("Error reading from file");
-        fclose(in);
-        return -1;
-    }
+int read_input_lists(const char *input_file_name, int max_list_length,
+                     int *list1, int *list2) {
+  FILE *in = fopen(input_file_name, "r");
+  if (in == NULL) {
+    perror("Could not open file");
+    return -1;
+  }
 
+  int n_lines_read = 0;
+  char line[MAX_INPUT_LINE_CHARS];
+  while (fgets(line, sizeof(line), in)) {
+    if (line_is_blank(line)) {
+      continue;
+    }
+    if (n_lines_read >= max_list_length) {
+      fprintf(stderr, "Input file exceeded maximum list length.\n");
+      fclose(in);
+      return -1;
+    }
+    int a, b;
+    if (sscanf(line, "%d %d", &a, &b) == 2) {
+      list1[n_lines_read] = a;
+      list2[n_lines_read] = b;
+      ++n_lines_read;
+    } else {
+      fprintf(stderr, "Could not read two int values from line.\n");
+      fclose(in);
+      return -1;
+    }
+  }
+  if (ferror(in)) {
+    perror("Error reading from file");
     fclose(in);
-    assert(n_lines_read < max_list_length);
-    return n_lines_read;
+    return -1;
+  }
+
+  fclose(in);
+  assert(n_lines_read < max_list_length);
+  return n_lines_read;
 }
 
 /**
@@ -147,12 +151,12 @@ int read_input_lists(
  * @see isspace
  */
 bool line_is_blank(const char *line) {
-    assert(line != NULL);
-    while (*line != '\0') {
-        if (!isspace(*line)) {
-            return false;
-        }
-        line++;
+  assert(line != NULL);
+  while (*line != '\0') {
+    if (!isspace(*line)) {
+      return false;
     }
-    return true;
+    line++;
+  }
+  return true;
 }
